949c8f9e760fa036bec1a36deedf456e169cbe93
Marc Hauschildt <marc.hauschildt@gmail.com>
2020-07-23 22:38:39 -0500

    Added query string search

--- server.js
+++ server.js
@@ -2,14 +2,17 @@
 
 // Application Dependencies
 require('dotenv').config();
-const express = require('express'); //
+const express = require('express');
+const cors = require('cors');
 
 // Application Setup
-const app = express(); //
-const PORT = process.env.PORT || 3000; //
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(cors());
 
 // Route Definitions
 app.get('/', rootHandler);
+app.get('/location', locationHandler);
 app.get('/bad', (request, response) => { throw new Error('Sorry the the inconvenience.') });
 app.use('*', notFoundHandler);
 app.use(errorHandler);
@@ -17,6 +20,13 @@ app.use(errorHandler);
 // Route Handlers
 function rootHandler(request, response) {
   response.status(200).send('City Explorer back-end');
+}
+
+function locationHandler(request, response) {
+  const city = request.query.city;
+  const locationData = require('./data/location.json');
+  const location = new Location(city, locationData);
+  response.status(200).send(location);
 }
 
 function notFoundHandler(request, response) {
@@ -27,5 +37,13 @@ function errorHandler(error, request, response, next) {
   response.status(500).json({ error: true, message: error.message });
 }
 
+// Constructors
+function Location(city, locationData) {
+  this.search_query = city;
+  this.formatted_query = locationData[0].display_name;
+  this.latitude = parseFloat(locationData[0].lat);
+  this.longitude = parseFloat(locationData[0].lon);
+}
+
 // App listener
-app.listen(PORT,() => console.log(`Listening on port ${PORT}`)); //
+app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
