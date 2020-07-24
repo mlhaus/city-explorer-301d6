23e5aa5315c3edb2f10676cb469daa9def94bae6
Marc Hauschildt <marc.hauschildt@gmail.com>
2020-07-23 21:47:36 -0500

    Removed unnecessary routes

--- server.js
+++ server.js
@@ -4,15 +4,12 @@
 require('dotenv').config();
 const express = require('express'); //
 
-// Application Setup 
+// Application Setup
 const app = express(); //
 const PORT = process.env.PORT || 3000; //
 
 // Route Definitions
-// app.use(express.static('./public'));
 app.get('/', rootHandler);
-app.get('/greet', greetHandler);
-app.get('/data', dataHandler);
 app.get('/bad', (request, response) => { throw new Error('Sorry the the inconvenience.') });
 app.use('*', notFoundHandler);
 app.use(errorHandler);
@@ -20,26 +17,6 @@ app.use(errorHandler);
 // Route Handlers
 function rootHandler(request, response) {
   response.status(200).send('City Explorer back-end');
-}
-
-function greetHandler(request, response) {
-  response.status(200).send('Hello World!');
-}
-
-function dataHandler(request, response) {
-  const airplanes = [
-    {
-      departure: new Date(2020, 7, 24),
-      canFly: true,
-      pilot: 'Well Trained'
-    },
-    {
-      departure: new Date(2020, 7, 25),
-      canFly: false,
-      pilot: 'NA'
-    }
-  ];
-  response.status(200).json(airplanes);
 }
 
 function notFoundHandler(request, response) {
