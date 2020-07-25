4f76e5ad1cd48fa58c6b7ad5d8f1442b1eb16277
Marc Hauschildt <marc.hauschildt@gmail.com>
2020-07-25 18:55:31 -0500

    Using LocationIQ API

--- server.js
+++ server.js
@@ -4,16 +4,17 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const superagent = require('superagent');
 
 // Application Setup
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.use(cors());
 
 // Route Definitions
 app.get('/', rootHandler);
 app.get('/location', locationHandler);
-app.get('/bad', (request, response) => { throw new Error('Sorry the the inconvenience.') });
+app.get('/yelp', restaurantHandler);
 app.use('*', notFoundHandler);
 app.use(errorHandler);
 
@@ -24,13 +25,41 @@ function rootHandler(request, response) {
 
 function locationHandler(request, response) {
   const city = request.query.city;
-  const locationData = require('./data/location.json');
-  const location = new Location(city, locationData);
-  response.status(200).send(location);
+  const url = 'https://us1.locationiq.com/v1/search.php';
+  superagent.get(url)
+    .query({
+      key: process.env.LOCATION_KEY,
+      q: city,
+      format: 'json'
+    })
+    .then(locationResponse => {
+      const geoData = locationResponse.body;
+      const location = new Location(city, geoData);
+      response.status(200).send(location);
+    })
+    .catch(err => {
+      console.log(err);
+      errorHandler(err, request, response);
+    });
+}
+
+function restaurantHandler(request, response) {
+  // const queryString = request.query;
+  // const lat = request.query.latitude;
+  // const lon = request.query.longitude;
+  // console.log(queryString);
+  // console.log(lat, lon);
+  const restaurantsData = require('./data/restaurants.json');
+  const arrayOfRestaurants = restaurantsData.nearby_restaurants;
+  const restaurantsResults = [];
+  arrayOfRestaurants.forEach(restaurantObj => {
+    restaurantsResults.push(new Restaurant(restaurantObj));
+  });
+  response.send(restaurantsResults);
 }
 
 function notFoundHandler(request, response) {
-  response.status(404).json({ notFound: true });
+  response.status(404).send('Not found');
 }
 
 function errorHandler(error, request, response, next) {
@@ -43,6 +72,14 @@ function Location(city, locationData) {
   this.formatted_query = locationData[0].display_name;
   this.latitude = parseFloat(locationData[0].lat);
   this.longitude = parseFloat(locationData[0].lon);
+}
+
+function Restaurant(obj) {
+  this.name = obj.restaurant.name;
+  this.url = obj.restaurant.url;
+  this.rating = obj.restaurant.user_rating.aggregate_rating;
+  this.price = obj.restaurant.price_range;
+  this.image_url = obj.restaurant.featured_image;
 }
 
 // App listener
